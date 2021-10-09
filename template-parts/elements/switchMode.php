46b1502581efa799a02026a1b846167b0a5fcc3a
EnzaEnzi <enza.webdev@gmail.com>
2021-10-09 17:23:10 +0200

    :construction: switchMode (html/css) :lipstick:

---
+++ template-parts/elements/switchMode.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Name file:   switchMode
+ * Description: Display the button that allows to change color theme [day mode or dark mode].
+ * Important:   This element is fixed on all pages of site
+ *
+ * @package WordPress
+ * @subpackage pekinparis
+ * @version 1.0
+ */
+ ?>
+
+
+<!-- <div class="toggles flip">
+  <input type="checkbox" name="switch" id="switch" />
+  <label for="switch"></label>
+</div> -->
+
+<div class="switcher-mode">
+  <input type="checkbox" class="checkbox" id="chk" />
+  <label class="label" for="chk">
+    <!-- Add fontt flaticon -->
+    <!-- <i class="fas fa-moon"></i> -->
+    <!-- <i class="fas fa-sun"></i> -->
+    <div class="ball"></div>
+  </label>
+</div>
