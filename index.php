3c9b36f1d3b0c00d2fac326397dea9f10f69db75
EnzaEnzi <enza.webdev@gmail.com>
2021-10-11 16:11:20 +0200

    :wrench: add index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Name file :   Index
+ * Description : The main template file
+ *
+ * @package WordPress
+ * @subpackage pekinparis
+ * @version 1.0
+ */
+?>
+
+<?php get_header() ?>
+<main>
+
+</main>
+<?php get_footer() ?>
