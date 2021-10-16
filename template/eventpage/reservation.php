bab4fe0128f2e29e27e24ab654916820c78d8014
EnzaEnzi <enza.webdev@gmail.com>
2021-10-17 01:18:20 +0200

    :building_construction: call-to-action

--- template/eventpage/reservation.php
+++ template/eventpage/reservation.php
@@ -12,20 +12,24 @@
 
 <?php if(checked(1, get_option('hidden_reservation_eventpage'), false)): else: ?>
 	<section class="reservation container">
-		<ul class="group-list">
-			<?php if(checked(1, get_option('add_icon_reservation_eventpage'), false)): ?>
-				<li class="group-item reserve-icon">
-					<i class="icons flaticon-telephone"></i>
-				</li>
-			<?php endif; ?>
-			<li class="group-item reserve-info">
-				<p class="reserve-message"><?php echo get_option('message_reservation_eventpage') ?></p>
-				<?php if(checked(1, get_option('add_phone_reservation_eventpage'), false)): ?>
-					<a href="tel:<?php get_option('phone') ?>" class="reserve-phone">
-						<?php echo get_option('phone') ?>
-					</a>
-				<?php endif; ?>
-			</li>
-		</ul>
+	<?php if(checked(1, get_option('add_phone_reservation_eventpage'), false)): ?>
+        <a  href="tel:<?php echo get_option('phone') ?>">
+            <ul class="group-list">
+                <?php if(checked(1, get_option('add_icon_reservation_eventpage'), false)): ?>
+                    <li class="group-item reserve-icon">
+                        <i class="icons flaticon-telephone"></i>
+                    </li>
+                <?php endif; ?>
+                <li class="group-item reserve-info">
+                    <p class="reserve-message">
+                        <?php echo get_option('message_reservation_eventpage') ?>
+                    </p>
+                    <p class="reserve-phone">
+                        <?php echo get_option('phone') ?>
+                    </p>
+                </li>
+            </ul>
+        </a>
+    <?php endif; ?>
 	</section>
 <?php endif; ?>
